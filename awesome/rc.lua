963c90c45961651f65ada324934b12203042bd6e
delta4d <delta4d@gmail.com>
2012-10-06 21:35:12 -0400

    awesome window manager config files

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -72,7 +72,7 @@ layouts =
 -- Define a tag table which hold all screen tags.
 --tags = {}
 --for s = 1, screen.count() do
-    -- Each screen has its own tag table.
+--	  Each screen has its own tag table.
 --    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
 --end
 -- }}}
